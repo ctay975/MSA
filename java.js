f1c8f01d4e182c974dd72ac201b0a0f0b5c339a6
Cameron Taylor <ctay975@aucklanduni.ac.nz>
2016-08-16 22:29:13 +1200

    CAMERON UPDATE 10.29PM 16/08/2016

--- java.js
+++ java.js
@@ -1,21 +1,33 @@
 var count = 0;
 var total;
+var results = [];
+var qno = 0;
+var outputstring = "<h3> Answers to Questions Above: </h3>";
 function answer() {
     //retrieve quiz answers in form
     var quiz1ans = document.forms["quiz1"].elements;
     var length = (quiz1ans.length - 1) / 4;
+    //QUESTION ONE
     var answer1 = quiz1ans["pouletfr"].value;
     if (answer1 == "Chicken") {
         count += 1;
+        results[qno] = "Correct";
     }
     else
-        ;
+        results[qno] = "Incorrect";
+    qno += 1;
+    //QUESTION TWO
     var answer2 = quiz1ans["ccva"].value;
     if (answer2 == "cvb") {
         count += 1;
+        results[qno] = "Correct";
     }
     else
-        ;
-    //output
-    alert("You Received " + count / length * 100 + "%");
+        results[qno] = "Incorrect";
+    //OUTPUT
+    var output = count / length * 100 + "%";
+    for (qno = 0; qno < length; qno += 1) {
+        outputstring += "Your Answer to Question " + (qno + 1) + " was " + results[qno] + "<br>";
+    }
+    document.getElementById("demo").innerHTML = outputstring;
 }
--- java.ts
+++ java.ts
@@ -1,5 +1,10 @@
 var count: number = 0;
 var total: number; 
+let results: string[] = [];
+let qno: number = 0;
+let outputstring: string = "<h3> Answers to Questions Above: </h3>"
+
+
 
 function answer(): void {
     
@@ -7,18 +12,27 @@ function answer(): void {
     var quiz1ans: string[] = document.forms["quiz1"].elements
     var length = (quiz1ans.length - 1)/4;
 
+    //QUESTION ONE
     var answer1: string = quiz1ans["pouletfr"].value;
     if (answer1 == "Chicken"){
         count += 1; 
+        results[qno]= "Correct";
     }
-    else;
-    
+    else results[qno] = "Incorrect";
+    qno += 1;
+
+    //QUESTION TWO
     var answer2: string = quiz1ans["ccva"].value;
     if (answer2 == "cvb" ){
         count += 1;
+        results[qno] = "Correct";
     }
-    else;
+    else results[qno] = "Incorrect";
 
-    //output
-    alert("You Received " + count/length * 100 + "%");
+    //OUTPUT
+    var output: string = count/length * 100 + "%";
+    for (qno = 0; qno < length; qno += 1){
+    outputstring += "Your Answer to Question " + (qno + 1) + " was " + results[qno] + "<br>";
+    }
+    document.getElementById("demo").innerHTML = outputstring;
 }
