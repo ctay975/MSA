74e4f7b67294a45f97fbdd761477886cc8b6ca7d
Cameron Taylor <ctay975@aucklanduni.ac.nz>
2016-08-17 19:01:51 +1200

    git add -A

--- java.js
+++ java.js
@@ -8,10 +8,12 @@ function answer() {
     //retrieve quiz answers in form
     var quiz1ans = document.forms["quiz1"].elements;
     var length = (quiz1ans.length - 1) / 4;
+    var correct = 0;
     //QUESTION ONE
     var answer1 = quiz1ans["pouletfr"].value;
     if (answer1 == "Chicken") {
         results[qno] = "Correct";
+        correct += 1;
     }
     else
         results[qno] = "Incorrect";
@@ -20,13 +22,25 @@ function answer() {
     var answer2 = quiz1ans["ccva"].value;
     if (answer2 == "cvb") {
         results[qno] = "Correct";
+        correct += 1;
     }
     else
         results[qno] = "Incorrect";
+    qno += 1;
+    //QUESTION THREE
+    var answer3 = quiz1ans["q3f"].value;
+    if (answer3 == "a4") {
+        results[qno] = "Correct";
+        correct += 1;
+    }
+    else
+        results[qno] = "Incorrect";
+    qno += 1;
     //OUTPUT
-    var output = count / length * 100 + "%";
+    var output = correct / length * 100 + "%";
     for (qno = 0; qno < length; qno += 1) {
         outputstring += "Your Answer to Question " + (qno + 1) + " was " + results[qno] + "<br>";
     }
+    outputstring += "<br><ul>You Final Mark is " + output + "<ul>";
     document.getElementById("demo").innerHTML = outputstring;
 }
--- java.ts
+++ java.ts
@@ -13,26 +13,41 @@ function answer(): void {
     //retrieve quiz answers in form
     var quiz1ans: string[] = document.forms["quiz1"].elements
     var length = (quiz1ans.length - 1)/4;
+    let correct: number = 0;
 
     //QUESTION ONE
     var answer1: string = quiz1ans["pouletfr"].value;
     if (answer1 == "Chicken"){
         results[qno]= "Correct";
+        correct += 1;
     }
     else results[qno] = "Incorrect";
     qno += 1;
 
     //QUESTION TWO
     var answer2: string = quiz1ans["ccva"].value;
-    if (answer2 == "cvb" ){
+    if (answer2 == "cvb"){
         results[qno] = "Correct";
+        correct += 1;
     }
     else results[qno] = "Incorrect";
+    qno += 1;
+
+    //QUESTION THREE
+    var answer3: string = quiz1ans["q3f"].value;
+    if (answer3 == "a4"){
+        results[qno] = "Correct";
+        correct += 1;
+    }
+    else results[qno] = "Incorrect";
+    qno += 1;
+
 
     //OUTPUT
-    var output: string = count/length * 100 + "%";
+    var output: string = correct/length * 100 + "%";
     for (qno = 0; qno < length; qno += 1){
     outputstring += "Your Answer to Question " + (qno + 1) + " was " + results[qno] + "<br>";
     }
+    outputstring += "<br><ul>You Final Mark is " + output + "<ul>";
     document.getElementById("demo").innerHTML = outputstring;
 }
