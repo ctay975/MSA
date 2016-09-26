9443f4ab1f10ca422b3b5f0c247e9d6e4dd29076
Cameron Taylor <ctay975@aucklanduni.ac.nz>
2016-09-26 16:05:27 +1300

    Final Update & Changes

--- java.js
+++ java.js
@@ -1,45 +1,42 @@
 //INITIAL DECLARATIONS
 var count = 0;
 var results = [];
-var qno = 0;
 //INITIAL FRENCH ANSWERS
 function answer() {
     var outputstring = "Answers to Questions Above: \n\n";
     //retrieve quiz answers in form
-    var quiz1ans = document.forms["quiz1"].elements;
-    var length = ((quiz1ans.length) - 1) / 4;
+    var quiz1ans = $("#quiz1 ol").children();
+    var length = ((quiz1ans.length) / 2);
     var correct = 0;
     //QUESTION ONE
-    var answer1 = quiz1ans["pouletfr"].value;
+    var answer1 = $("input[name='pouletfr']:checked").val();
     if (answer1 == "Chicken") {
-        results[qno] = "Correct";
+        results.push("Correct");
         correct += 1;
     }
-    else
-        results[qno] = "Incorrect";
-    qno += 1;
+    else {
+        results.push("Incorrect");
+    }
     //QUESTION TWO
-    var answer2 = quiz1ans["ccva"].value;
+    var answer2 = $("input[name='ccva']:checked").val();
     if (answer2 == "cvb") {
-        results[qno] = "Correct";
+        results.push("Correct");
         correct += 1;
     }
     else
-        results[qno] = "Incorrect";
-    qno += 1;
+        results.push("Incorrect");
     //QUESTION THREE
-    var answer3 = quiz1ans["q3f"].value;
+    var answer3 = $("input[name='q3f']:checked").val();
     if (answer3 == "a4") {
-        results[qno] = "Correct";
+        results.push("Correct");
         correct += 1;
     }
     else
-        results[qno] = "Incorrect";
-    qno += 1;
+        results.push("Incorrect");
     //OUTPUT
     var output = correct / length * 100;
-    for (qno = 0; qno < length; qno += 1) {
-        outputstring += " Your Answer to Question " + (qno + 1) + " was " + results[qno] + "\n";
+    for (count = 0; count < length; count++) {
+        outputstring += " Your Answer to Question " + (count + 1) + " was " + results[count] + "\n";
     }
     alert(outputstring);
     alert("Your final mark of " + Math.round(output) + "% has been submitted");
--- java.ts
+++ java.ts
@@ -1,8 +1,6 @@
 //INITIAL DECLARATIONS
 let count: number = 0;
 let results: string[] = [];
-let qno: number = 0;
-
 
 
 //INITIAL FRENCH ANSWERS
@@ -11,42 +9,41 @@ function answer(): void {
 
     let outputstring: string = "Answers to Questions Above: \n\n"; 
     //retrieve quiz answers in form
-    var quiz1ans: string[] = document.forms["quiz1"].elements
-    var length: number = ((quiz1ans.length)-1)/4;
+    var quiz1ans: string[] = $("#quiz1 ol").children();
+    var length: number = ((quiz1ans.length)/2);
     let correct: number = 0;
 
     //QUESTION ONE
-    var answer1: string = quiz1ans["pouletfr"].value;
+    var answer1: string = $("input[name='pouletfr']:checked").val();
     if (answer1 == "Chicken"){
-        results[qno]= "Correct";
+        results.push("Correct");
         correct += 1;
     }
-    else results[qno] = "Incorrect";
-    qno += 1;
+    else {
+        results.push("Incorrect");
+    }
 
     //QUESTION TWO
-    var answer2: string = quiz1ans["ccva"].value;
+    var answer2: string = $("input[name='ccva']:checked").val();
     if (answer2 == "cvb"){
-        results[qno] = "Correct";
+        results.push("Correct");
         correct += 1;
     }
-    else results[qno] = "Incorrect";
-    qno += 1;
+    else results.push("Incorrect");
 
     //QUESTION THREE
-    var answer3: string = quiz1ans["q3f"].value;
+    var answer3: string = $("input[name='q3f']:checked").val();
     if (answer3 == "a4"){
-        results[qno] = "Correct";
+        results.push("Correct");
         correct += 1;
     }
-    else results[qno] = "Incorrect";
-    qno += 1;
+    else results.push("Incorrect");
 
 
     //OUTPUT
     var output: number = correct/length * 100;
-    for (qno = 0; qno < length; qno += 1){
-    outputstring += " Your Answer to Question " + (qno + 1) + " was " + results[qno] + "\n";
+    for (count = 0; count < length; count ++){
+    outputstring += " Your Answer to Question " + (count + 1) + " was " + results[count] + "\n";
     }
     alert(outputstring);
     alert("Your final mark of " + Math.round(output) + "% has been submitted");
